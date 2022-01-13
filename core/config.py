a0a06665073d7539161da063471ad0d4b49be3a7
binogood <binogood68@gmail.com>
2022-01-13 14:52:03 +0900

    Add: 설정 파일 추가 - main.py - config.py

---
+++ core/config.py
@@ -0,0 +1,34 @@
+import os
+
+from pydantic import BaseSettings
+from my_settings import DB_URL
+
+
+class Config(BaseSettings):
+    ENV: str = "development"
+    DEBUG: bool = True
+    APP_HOST: str = "0.0.0.0"
+    APP_POST: int = 8000
+    DB_URL: str = f"mysql+pymysql://{DB_URL['user']}:{DB_URL['password']}@{DB_URL['host']}:{DB_URL['port']}/{DB_URL['db']}"
+    JWT_SECRET_KEY: str = "payhere"
+    JWT_ALGORITHM: str = "HS256"
+
+
+class DevelopmentConfig(Config):
+    DB_URL: str = f"mysql+pymysql://{DB_URL['user']}:{DB_URL['password']}@{DB_URL['host']}:{DB_URL['port']}/{DB_URL['db']}"
+
+
+class LocalConfig(Config):
+    DB_URL: str = f"mysql+pymysql://{DB_URL['user']}:{DB_URL['password']}@{DB_URL['host']}:{DB_URL['port']}/{DB_URL['db']}"
+
+
+def get_config():
+    env = os.getenv("ENV", "development")
+    config_type = {
+        "development": DevelopmentConfig(),
+        "local": LocalConfig(),
+    }
+    return config_type[env]
+
+
+config = get_config()
