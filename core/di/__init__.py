b15085257e3609d2c4726f9eb412e56241d2a45b
binogood <binogood68@gmail.com>
2022-01-17 13:01:18 +0900

    Update: pythondi 파일 설정 추가 - household_ledger 설정 추가

--- core/di/__init__.py
+++ core/di/__init__.py
@@ -1,10 +1,14 @@
 from pythondi import Provider, configure
-# from app.user.repository.user import UserRepo, UserMySQLRepo
+from app.user.repository.user import UserRepo, UserMySQLRepo
+from app.household_ledger.repository.household_ledger import (
+    HouseholdLedgerRepo,
+    HouseHoldLedgerMySQLRepo
+)
 
 
 
 def init_di():
     provider = Provider()
-    # provider.bind(UserRepo, UserMySQLRepo)
+    provider.bind(UserRepo, UserMySQLRepo)
+    provider.bind(HouseholdLedgerRepo, HouseHoldLedgerMySQLRepo)
     configure(provider=provider)
-    pass
