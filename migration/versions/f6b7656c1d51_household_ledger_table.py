febd13131b2a481da9598a22ba0b08262db97819
binogood <binogood68@gmail.com>
2022-01-17 12:55:57 +0900

    Add: household_ledger.py 재생성

--- migration/versions/f6b7656c1d51_household_ledger_table.py
+++ migration/versions/f6b7656c1d51_household_ledger_table.py
@@ -1,8 +1,8 @@
-"""financial_ledger_table
+"""household_ledger_table
 
-Revision ID: 51f5616ffcb3
+Revision ID: f6b7656c1d51
 Revises: 84d2d97d39a9
-Create Date: 2022-01-12 15:06:44.934031
+Create Date: 2022-01-15 14:29:27.944357
 
 """
 from alembic import op
@@ -10,25 +10,26 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '51f5616ffcb3'
-down_revision = '8b6d2543dcbb'
+revision = 'f6b7656c1d51'
+down_revision = None
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
-    op.create_table('household_ledgers',
+    op.create_table('household_ledger',
         sa.Column('id', sa.Integer(), autoincrement=True, nullable=False),
         sa.Column('user_id', sa.Integer(), nullable=False),
-        sa.Column('transfer_id', sa.Integer(), nullable=False),
         sa.Column('memo', sa.String(length=100), nullable=False),
-        sa.Column('price', sa.Numeric(14,2), nullable=False),
+        sa.Column('transfer', sa.String(length=20), nullable=False),
+        sa.Column('price', sa.Numeric(14, 2), nullable=False),
         sa.Column('created_at', sa.DateTime(), nullable=False),
+        sa.Column('updated_at', sa.DateTime(), nullable=False),
         sa.Column('is_delete', sa.Boolean(), default=False, nullable=False),
-        sa.UniqueConstraint('name'),
+        sa.ForeignKeyConstraint(['user_id'], ['users.id'], ondelete='cascade'),
         sa.PrimaryKeyConstraint('id'),
     )
 
 
 def downgrade():
-    op.drop_table('household_ledgers')
+    op.drop_table('household_ledger')
